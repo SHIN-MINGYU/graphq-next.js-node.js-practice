e185d5a4ff58b4a4bebe3dc00f8930668eee0610
SHIN_MINGYU <right7066@naver.com>
2022-06-19 02:49:15 +0900

    mongoDB connect

--- index.ts
+++ index.ts
@@ -4,11 +4,14 @@ import path from "path";
 import { readFileSync } from "fs";
 import express from "express";
 import http from "http";
+import connect from "./schemas";
 import resolvers from "./graphql/resolvers";
 
 const typeDefs = readFileSync(
-  require.resolve("./graphql/typeDefs.graphql")
+  require.resolve(path.join(__dirname, "./graphql/typeDefs.graphql"))
 ).toString();
+
+connect();
 
 async function startApolloServer(typeDefs: any, resolvers: any) {
   const app = express();
@@ -20,15 +23,19 @@ async function startApolloServer(typeDefs: any, resolvers: any) {
     cache: "bounded",
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-  await server.start();
-  server.applyMiddleware({
-    app,
-    path: "/",
-  });
-  await new Promise<void>((resolve) =>
-    httpServer.listen({ port: 4000 }, resolve)
-  );
-  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+  try {
+    await server.start();
+    server.applyMiddleware({
+      app,
+      path: "/",
+    });
+    await new Promise<void>((resolve) =>
+      httpServer.listen({ port: 4000 }, resolve)
+    );
+    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 startApolloServer(typeDefs, resolvers);
---
+++ schemas/index.ts
@@ -0,0 +1,35 @@
+import mongoose, { ConnectOptions } from "mongoose";
+import { config } from "dotenv";
+
+config();
+
+const connect = () => {
+  if (process.env.NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+};
+mongoose.connect(
+  process.env.DB_HOST!,
+  {
+    dbName: "the_forest",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions,
+  (err) => {
+    if (err) {
+      console.log("몽고디비 연결 에러 ", err);
+    } else {
+      console.log("몽고디비 연결 성공");
+    }
+  }
+);
+
+mongoose.connection.on("error", (err) => {
+  console.error("몽고디비 연결 에러", err);
+});
+mongoose.connection.on("disconnected", () => {
+  console.error("몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.");
+  connect();
+});
+
+export default connect;
