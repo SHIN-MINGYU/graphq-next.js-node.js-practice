ac1cb768b4f89032bf2e9aecc63c16abea01cb70
unknown <smg20004@naver.com>
2022-07-28 06:07:39 +0900

    cache clear and execute program nodemon => pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    apps: [
+        {
+            name: 'the_forest_server', // pm2 name
+            script: './index.ts', // // app execute file path
+            instances: 3, // cluster mode instance volume
+            exec_mode: 'cluster', // fork or cluster mode 
+            merge_logs: true, // if cluster mode, several clusters log merge
+            autorestart: true, // if process failed, restart access
+            node_args: '-r ts-node -r tsconfig-paths/register', // args for compiler
+            watch: true, // if file is changed, restart access 
+            // max_memory_restart: "512M",
+            env: {
+                // development env
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                // production env (can assign --env production option.)
+                NODE_ENV: 'production',
+            }
+        },
+    ]
+};
